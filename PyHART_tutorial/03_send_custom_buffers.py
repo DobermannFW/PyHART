0f0db5876b1405dc25a8c185dc0a20914987031b
DobermannFW <73604269+DobermannFW@users.noreply.github.com>
2022-02-22 15:43:40 +0100

    Update 03_send_custom_buffers.py

--- PyHART_tutorial/03_send_custom_buffers.py
+++ PyHART_tutorial/03_send_custom_buffers.py
@@ -20,7 +20,7 @@ sys.path.append('../')
 from PyHART.COMMUNICATION.CommCore import *
 from PyHART.COMMUNICATION.Utils import *
 from PyHART.COMMUNICATION.Packet import *
-from PyHART.COMMUNICATION.System import *
+from PyHART.COMMUNICATION.Common import *
 
 #
 # Procedure to list communication ports
